1e20fce8eae02a403eb70fbe042147c010bbf0df
Alexander Bogdanov <syn@li.ru>
2012-07-02 01:49:46 +0400

    Default action for migrations

---
+++ classes/task/migrations.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Displays the current status of migrations in all groups
+ *
+ * This task takes no config options
+ *
+ * @author Matt Button <matthew@sigswitch.com>
+ */
+class Task_Migrations extends Task_Migrations_Status
+{
+
+}
