27bd05b7045967340611c444301053936e6f55f2
Alexander Bogdanov <syn@li.ru>
2013-09-20 13:42:45 +0400

    Making migrations status view Notice-safe

--- views/minion/task/migrations/status.php
+++ views/minion/task/migrations/status.php
@@ -1,10 +1,10 @@
 <?php foreach($groups as $group => $status): ?>
  >> <?php echo $group?>
 
-    Current version: <?php echo $status['timestamp'] !== null ?
+    Current version: <?php echo Arr::get($status,'timestamp') !== null ?
         sprintf('%s (%d)',Date::formatted_time($status['timestamp']), $status['timestamp']) :
         'No applied migrations'
-    ?> <?php echo $status['description']?>
+    ?> <?php echo Arr::get($status,'description')?>
 
     Available migrations: <?php echo $status['count_available']?>
 
